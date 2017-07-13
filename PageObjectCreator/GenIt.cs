f099c7c31ceb4cf32fd4dddf33283284caaaeccc
QAAutomationRules <avzrule@gmail.com>
2017-07-13 11:50:40 -0600

    initial checkin

---
+++ PageObjectCreator/GenIt.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Abot.Crawler;
+using Abot.Poco;
+using CsQuery.ExtensionMethods;
+using HtmlAgilityPack;
+
+namespace PageObjectCreator
+{
+    [TestClass]
+    public class GenIt
+    {
+        [TestMethod]
+        public void GenStuff()
+        {
+            PoliteWebCrawler crawler = new PoliteWebCrawler();
+            
+            crawler.PageCrawlStartingAsync += crawler_ProcessPageCrawlStarting;
+            crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
+            crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowed;
+            crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed;
+
+            CrawlResult result = crawler.Crawl(new Uri("https://www.mysmartmove.com/"));
+
+            int count = result.CrawlContext.CrawledCount;
+
+            Console.WriteLine(result.CrawlContext.ToJSON());
+
+            Console.WriteLine(result.ToJSON());
+
+            foreach (var tag in GetPageElements())
+            {
+                Console.WriteLine(tag);
+            }
+        }
+
+        void crawler_ProcessPageCrawlStarting(object sender, PageCrawlStartingArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            Console.WriteLine("About to crawl link {0} which was found on page {1}", pageToCrawl.Uri.AbsoluteUri, pageToCrawl.ParentUri.AbsoluteUri);
+        }
+
+        void crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            List<string> webPages = new List<string>();
+
+            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+                Console.WriteLine("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
+            else
+                Console.WriteLine("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
+                webPages.Add(crawledPage.Uri.AbsoluteUri);
+                Console.WriteLine(crawledPage.Uri.AbsoluteUri);
+            if (string.IsNullOrEmpty(crawledPage.Content.Text))
+                Console.WriteLine("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
+
+            var htmlAgilityPackDocument = crawledPage.HtmlDocument; //Html Agility Pack parser
+            var angleSharpHtmlDocument = crawledPage.AngleSharpHtmlDocument; //AngleSharp parser
+
+            //return webPages;
+        }
+
+        void crawler_PageLinksCrawlDisallowed(object sender, PageLinksCrawlDisallowedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            Console.WriteLine("Did not crawl the links on page {0} due to {1}", crawledPage.Uri.AbsoluteUri, e.DisallowedReason);
+        }
+
+        void crawler_PageCrawlDisallowed(object sender, PageCrawlDisallowedArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            Console.WriteLine("Did not crawl page {0} due to {1}", pageToCrawl.Uri.AbsoluteUri, e.DisallowedReason);
+        }
+
+
+        public List<string> GetPageElements()
+        {
+            List<string> tagsList = new List<string>();
+
+            string url = "https://www.mysmartmove.com/";
+
+            var Webget = new HtmlWeb();
+
+            var doc = Webget.Load(url);
+
+            try
+            {
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//h1"))
+                {
+                    tagsList.Add(node.ChildNodes[0].InnerHtml);
+                }
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//li"))
+                {
+                    tagsList.Add(node.ChildNodes[0].InnerHtml);
+                }
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//a"))
+                {
+                    tagsList.Add(node.ChildNodes[0].InnerHtml);
+                }
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//p"))
+                {
+                    tagsList.Add(node.ChildNodes[0].InnerHtml);
+                }
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//select"))
+                {
+                    tagsList.Add(node.ChildNodes[0].InnerHtml);
+                }
+                foreach (HtmlNode node in doc.DocumentNode.SelectNodes("//button"))
+                {
+                    tagsList.Add(node.ChildNodes[0].InnerHtml);
+                }
+            }
+            catch (Exception ex)
+            {            
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            return tagsList;
+        }
+    }
+}
